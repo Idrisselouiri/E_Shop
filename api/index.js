6baca28dcf15eb48b35ff860af23ffee490a4a87
ayoubtahir <ayoub.tahir.2021@gmail.com>
2024-03-15 12:07:11 +0000

    add listen server and connect mongoDb

---
+++ api/index.js
@@ -0,0 +1,33 @@
+import mongoose from "mongoose";
+import express from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
+// connect mongoDb
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("MongoDb is connected");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+const app = express();
+
+app.use(express.json());
+
+//listen server on port 3000
+app.listen(3000, () => {
+  console.log("Sever is running on port 3000");
+});
+
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Interval Server Error";
+  res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  });
+});
